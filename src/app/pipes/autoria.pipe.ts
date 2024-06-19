9d77eca0619d0ae349066e78f28081d626a4acab
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-06-19 17:19:42 -0300

    DONE custom pipe

--- src/app/pipes/autoria.pipe.ts
+++ src/app/pipes/autoria.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AutoriaPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(autoria: string[]): string {
+    if (autoria) {
+      return autoria[0];
+    }
+
+    return ''
   }
 
 }
