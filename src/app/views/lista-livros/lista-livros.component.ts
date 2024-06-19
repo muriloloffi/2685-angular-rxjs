1def5a68744ce730b8793f023bba80cc20fa6f7a
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-06-19 14:37:46 -0300

    WIP refactor lista-livro

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { Livro } from 'src/app/models/interfaces';
+import { Item, Livro } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -26,23 +27,26 @@ export class ListaLivrosComponent implements OnDestroy {
     });
   }
 
-  livrosResultadoParaLivros(items): Livro[] {
-    const livros: Livro[] = [];
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item);
+    })
+    // const livros: Livro[] = [];
 
     // Em cada posição (item) do array items, cria um objeto livro propriamente dito (duck typed) e atribui ao livro desta classe (this.livro). Em seguida, insere o valor desse atributo no array livros por meio do método push. O array será retornado ao fim deste método.
-    items.forEach(item => {
-      livros.push(this.livro = {
-        title: item.volumeInfo?.title,
-        authors: item.volumeInfo?.authors,
-        publisher: item.volumeInfo?.publisher,
-        publishedDate: item.volumeInfo?.publishedDate,
-        description: item.volumeInfo?.description,
-        previewLink: item.volumeInfo?.previewLink,
-        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
-      })
-    });
+    // items.forEach(item => {
+    //   livros.push(this.livro = {
+    //     title: item.volumeInfo?.title,
+    //     authors: item.volumeInfo?.authors,
+    //     publisher: item.volumeInfo?.publisher,
+    //     publishedDate: item.volumeInfo?.publishedDate,
+    //     description: item.volumeInfo?.description,
+    //     previewLink: item.volumeInfo?.previewLink,
+    //     thumbnail: item.volumeInfo?.imageLinks?.thumbnail
+    //   })
+    // });
 
-    return livros;
+    // return livros;
   }
 
   ngOnDestroy() {
