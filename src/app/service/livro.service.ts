ec90560fe5e75bb059b65479b93d89a98b718488
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-06-21 16:29:10 -0300

    WIP dynamic search - why would you throw another error after catching it?

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -7,24 +7,12 @@ import { Item, LivrosResultado } from '../models/interfaces';
   providedIn: 'root',
 })
 export class LivroService {
-
   private readonly API = 'https://www.googleapis.com/books/v1/volumes';
 
   constructor(private http: HttpClient) {}
 
-  buscar(valorDigitado: string): Observable<Item[]> {
-    if (valorDigitado) {
-      const params = new HttpParams().append('q', valorDigitado);
-      return this.http.get<LivrosResultado>(this.API, { params }).pipe(
-        map(resultado => resultado.items),
-        catchError(error => {
-          console.error('Erro na busca: ', error);
-          // return of([]);
-          return throwError(() => new Error('Ops, ocorreu um erro'))
-        })
-      )
-    } else {
-      return of([]);
-    }
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
+    const params = new HttpParams().append('q', valorDigitado);
+    return this.http.get<LivrosResultado>(this.API, { params });
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -7,10 +7,12 @@ import {
   distinctUntilChanged,
   filter,
   map,
+  of,
   switchMap,
   tap,
+  throwError,
 } from 'rxjs';
-import { Item } from 'src/app/models/interfaces';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -24,26 +26,49 @@ const PAUSA = 300;
 export class ListaLivrosComponent {
   campoBusca = new FormControl();
   mensagemErro = '';
+  livrosResultado: LivrosResultado;
 
   constructor(private service: LivroService) {}
 
+  totalDeLivros$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map(resultado => this.livrosResultado = resultado),
+    catchError(erro => {
+      console.log(erro);
+      return of()
+    })
+  )
+
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+
     //a ordem dos operadores abaixo é importante
     //delays the execution of the pipe method
     debounceTime(PAUSA),
+
     //stops if the search term is smaller than 3
     filter((valorDigitado) => valorDigitado.length >= 3),
+
     //doesn't send a new request to api if the search query is the same
     distinctUntilChanged(),
+
     //atualiza a saída, mas interrompe a requisição se o valor de query muda
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+
     //debug message
     tap(() => console.log('Requisição ao servidor')),
+    map(resultado => resultado.items ?? []),
+
     //atualiza a saída com base no callback provido
     map((items: Item[]) => this.livrosResultadoParaLivros(items)),
-    catchError(erro => {
-      this.mensagemErro = 'Ops, ocorreu um erro. Recarregue a aplicação.';
-      return EMPTY;
+    
+    catchError((erro) => {
+      // this.mensagemErro = 'Ops, ocorreu um erro. Recarregue a aplicação.';
+      // return EMPTY;
+      console.log(erro);
+      return throwError(() => new Error(this.mensagemErro = "Ops, ocorreu um erro. Recarregue a aplicação!"))
     })
   );
 
