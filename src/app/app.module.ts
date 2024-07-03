c8b1f513d85ddbecc30c9776e2f97f05b1264ecf
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-07-03 17:00:03 -0300

    WIP book list animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -13,6 +13,7 @@ import {
 import { Item, LivrosResultado } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
+import { trigger, transition, query, style, stagger, animate, keyframes } from '@angular/animations';
 
 const PAUSA = 300;
 
@@ -20,6 +21,31 @@ const PAUSA = 300;
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* <=> *', [
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateY(-50px)' }),
+          stagger('100ms', [
+            animate('500ms ease-out', keyframes([
+              style({ opacity: 0, transform: 'translateY(-50px)', offset: 0 }),
+              style({ opacity: 0.5, transform: 'translateY(-25px)', offset: 0.3 }),
+              style({ opacity: 1, transform: 'none', offset: 1 }),
+            ])),
+          ]),
+        ], { optional: true }),
+        query(':leave', [
+          stagger('100ms', [
+            animate('500ms ease-out', keyframes([
+              style({ opacity:1 , transform: 'none', offset: 0 }),
+              style({ opacity: 0.5, transform: 'translateY(-25px)', offset: 0.3 }),
+              style({ opacity: 0, transform: 'translateY(-50px)', offset: 1 })
+            ]))
+          ])
+        ], { optional: true }),
+      ]),
+    ]),
+  ],
 })
 export class ListaLivrosComponent {
   campoBusca = new FormControl();
