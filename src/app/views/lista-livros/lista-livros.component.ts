4cdd20ac98a85c3b41fefd1b082ba945190149d0
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-07-04 08:57:26 -0300

    Format animations

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -13,7 +13,15 @@ import {
 import { Item, LivrosResultado } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
-import { trigger, transition, query, style, stagger, animate, keyframes } from '@angular/animations';
+import {
+  trigger,
+  transition,
+  query,
+  style,
+  stagger,
+  animate,
+  keyframes,
+} from '@angular/animations';
 
 const PAUSA = 300;
 
@@ -24,25 +32,55 @@ const PAUSA = 300;
   animations: [
     trigger('listAnimation', [
       transition('* <=> *', [
-        query(':enter', [
-          style({ opacity: 0, transform: 'translateY(-50px)' }),
-          stagger('100ms', [
-            animate('500ms ease-out', keyframes([
-              style({ opacity: 0, transform: 'translateY(-50px)', offset: 0 }),
-              style({ opacity: 0.5, transform: 'translateY(-25px)', offset: 0.3 }),
-              style({ opacity: 1, transform: 'none', offset: 1 }),
-            ])),
-          ]),
-        ], { optional: true }),
-        query(':leave', [
-          stagger('100ms', [
-            animate('500ms ease-out', keyframes([
-              style({ opacity:1 , transform: 'none', offset: 0 }),
-              style({ opacity: 0.5, transform: 'translateY(-25px)', offset: 0.3 }),
-              style({ opacity: 0, transform: 'translateY(-50px)', offset: 1 })
-            ]))
-          ])
-        ], { optional: true }),
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(-50px)' }),
+            stagger('100ms', [
+              animate(
+                '500ms ease-out',
+                keyframes([
+                  style({
+                    opacity: 0,
+                    transform: 'translateY(-50px)',
+                    offset: 0,
+                  }),
+                  style({
+                    opacity: 0.5,
+                    transform: 'translateY(-25px)',
+                    offset: 0.3,
+                  }),
+                  style({ opacity: 1, transform: 'none', offset: 1 }),
+                ])
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
+        query(
+          ':leave',
+          [
+            stagger('100ms', [
+              animate(
+                '500ms ease-out',
+                keyframes([
+                  style({ opacity: 1, transform: 'none', offset: 0 }),
+                  style({
+                    opacity: 0.5,
+                    transform: 'translateY(-25px)',
+                    offset: 0.3,
+                  }),
+                  style({
+                    opacity: 0,
+                    transform: 'translateY(-50px)',
+                    offset: 1,
+                  }),
+                ])
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
       ]),
     ]),
   ],
